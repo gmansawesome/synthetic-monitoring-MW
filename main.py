237c7216b407f0274bf6143cc2cdcd45ec539134
Michael Wang <michaelwang1245@gmail.com>
2024-04-11 08:34:15 -0700

    implemented working config.yml reader

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import json
 import pingparsing
 from prometheus_client import start_http_server, Gauge
+import yaml
 
-# TO DO: implement reading from config.yml...
-# TO DO: setup basic working gauge
+def read_config(config_path): 
+    try:
+        with open(config_path, 'r') as file:
+            config_data = yaml.safe_load(file)
+        return config_data
 
-ping_latency_avg = Gauge('ping_latency_avg_ms', 'Average round-trip time (RTT) in milliseconds')
-ping_packet_loss_rate = Gauge('ping_packet_loss_rate', 'Ping packet loss rate')
+    except FileNotFoundError:
+        print("File not found!")
 
-ping_parser = pingparsing.PingParsing()
-transmitter = pingparsing.PingTransmitter()
-transmitter.destination = "google.com"
-transmitter.count = 10
-result = transmitter.ping()
+    except yaml.YAMLError as error:
+        print("Error occured!")
+        print(error)
 
-parsed_data = ping_parser.parse(result).as_dict()
 
-ping_latency_avg.set(4.2).labels("/fooBar")
-ping_packet_loss_rate.set(4.2).labels("/fooBar")
+def main():
+    config_data = read_config("config.yml")
+    print(config_data)
 
-start_http_server(8000)
+    # ping_latency_avg = Gauge('ping_latency_avg_ms', 'Average round-trip time (RTT) in milliseconds')
+    # ping_packet_loss_rate = Gauge('ping_packet_loss_rate', 'Ping packet loss rate')
 
-print(json.dumps(parsed_data, indent=4))
+    # ping_parser = pingparsing.PingParsing()
+    # transmitter = pingparsing.PingTransmitter()
+    # transmitter.destination = "google.com"
+    # transmitter.count = 10
+    # result = transmitter.ping()
+
+    # parsed_data = ping_parser.parse(result).as_dict()
+
+    # ping_latency_avg.set(4.2).labels("/fooBar")
+    # ping_packet_loss_rate.set(4.2).labels("/fooBar")
+
+    # start_http_server(8000)
+
+    # print(json.dumps(parsed_data, indent=4))
+
+main()
